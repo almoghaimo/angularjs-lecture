21f0bca436e3aba91579cedb3660c68fbb399d81
Almog <almoghaimo@gmail.com>
2020-11-17 01:30:22 +0200

    stage 5

--- ng-hello-module.js
+++ ng-hello-module.js
@@ -5,25 +5,24 @@ const {react2angular} = require('react2angular');
 
 const ngHelloModule = angular.module('helloModule', []);
 
-ngHelloModule.component('reactHelloComponent', react2angular(ReactHello, ['message', 'onMessageChange']));
+ngHelloModule.component('reactHelloComponent', react2angular(ReactHello, ['message', 'onMessageChange', 'almog']))
 
-const reactTemplate = `<div>
-<div>Angular: <input type="text" ng-model="message" ></div>
-<react-hello-component message="message" on-message-change="onReactMessageChange">
-</div>`;
-
-ngHelloModule.directive('helloDirective', ['$compile', function ($compile) {
+ngHelloModule.directive('helloDirective', function () {
   return {
-    template: `</div>`,
-link(scope, element, _, ctrl) {
-      console.log("link");
-      scope.message = 'Hello';
-      scope.onReactMessageChange = (message) => {
+    controller: function ($scope) {
+      $scope.message = 'Hello';
+      $scope.onReactMessageChange = (message) => {
         $scope.message = message;
         $scope.$apply();
       };
-      const el = $compile(reactTemplate)(scope);
-      element.parent().append(el);
+    },
+    template: `<div>
+      <div>Angular: <input type="text" ng-model="message" ></div>
+      <react-hello-component message="message" on-message-change="onReactMessageChange">
+      </div>`,
+    link(scope, element, _, ctrl) {
+      console.log("link");
+      scope.message = 'Hello from link';
     }
   };
-}]);
+});
