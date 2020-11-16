79390c30155ebd685f201259401dd3f525f12aa2
Almog <almoghaimo@gmail.com>
2020-11-17 00:43:28 +0200

    stage 3

--- entry.js
+++ entry.js
@@ -1,31 +1,4 @@
 import angular from 'angular';
 import './ng-hello-module';
 
-const entry = angular.module('app', ['helloModule']);
-
-// var app = angular.module('app', []);
-
-function createDirective(name){
-  return function(){
-    return {
-      restrict: 'E',
-      compile: function(tElem, tAttrs){
-        // In AngularJS the original DOM is often referred to as the the template element, which stands for template element.
-        console.log(`${name} : compile => ${tElem.html()}`);
-        return {
-          pre: function(scope, iElem, iAttrs){
-            scope.name2 = 'pre link';
-            console.log(`${name} : pre link => ${iElem.html()}`);
-          },
-          post: function(scope, iElem, iAttrs){
-            console.log(`${name} : post link => ${iElem.html()}`);
-          }
-        }
-      }
-    }
-  }
-}
-
-entry.directive('levelOne', createDirective('levelOne'));
-entry.directive('levelTwo', createDirective('levelTwo'));
-entry.directive('levelThree', createDirective('levelThree'));
+angular.module('app', ['helloModule']);
--- react-hello.jsx
+++ react-hello.jsx
@@ -3,16 +3,18 @@ import React, {Component} from 'react';
 class ReactHello extends Component {
     constructor(props) {
         super(props);
+        console.log("-------constructor--------");
         console.log(props);
     }
 
     componentDidMount() {
-        console.log("componentDidMount", this.props);
+        console.log("------componentDidMount-------");
+        console.log(this.props);
     }
 
-    componentDidUpdate(prevProps, props) {
-        console.log("almog prevProps: ", prevProps);
-        console.log("almog props: ", props);
+    componentDidUpdate(prevProps) {
+        console.log("------componentDidUpdate-------");
+        console.log(this.props);
     }
 
     render() {
